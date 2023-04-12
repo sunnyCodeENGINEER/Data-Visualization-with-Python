66c623bcf9e498d5ffb09b3f05cb3c90f654081f
sunnyCodeENGINEER <116773072+sunnyCodeENGINEER@users.noreply.github.com>
2023-04-12 00:02:06 +0000

    First setup for Hello Seaborn.

--- Hello Seaborn.py
+++ Hello Seaborn.py
@@ -2,6 +2,22 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-data = pd.read_csv("melb_data.csv")
+data = pd.read_csv("fifa.csv")
+print(data.head())
+print(data.columns)
 
+list_cols = ['ARG', 'BRA']
+
+print(list_cols)
+
+# Set the width and height of the figure
+plt.figure(figsize=(16, 6))
+
+# Add title
+plt.title("FIFA World Rankings for 6 countries")
+
+sns.lineplot(data=data['ARG'], label="Argentina")
+sns.lineplot(data=data['BRA'], label="Brazil")
+
+plt.show()
 

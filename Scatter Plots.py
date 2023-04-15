4e08f682ac9655393829b0e4c6f5f9ea07390d86
sunnyCodeENGINEER <116773072+sunnyCodeENGINEER@users.noreply.github.com>
2023-04-15 02:31:54 +0000

    Scatter Plots.

--- Scatter Plots.py
+++ Scatter Plots.py
@@ -2,5 +2,30 @@ import pandas as pd
 import seaborn as sn
 import matplotlib.pyplot as plt
 
+data = pd.read_csv("insurance.csv")
+print(data.head())
+print(data.columns)
+
+sn.scatterplot(x=data.bmi, y=data.charges)
+
+plt.show()
+
+sn.regplot(x=data.bmi, y=data.charges)
+
+plt.show()
+
+sn.scatterplot(x=data.bmi, y=data.charges, hue=data.smoker)
+
+plt.show()
+
+sn.lmplot(x="bmi", y="charges", hue="smoker", data=data)
+
+plt.show()
+
+sn.swarmplot(x=data['smoker'],
+              y=data['charges'])
+
+plt.show()
+
 
 
